b9617d7db693b3b1ae8c5dcfef175d992049a8df
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2021-04-25 20:13:46 +0530

    Styles merged in pages

--- pages/Events/index.jsx
+++ pages/Events/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import events from "../../data/events.json"
-import {Event, Title ,Img, Cardrev, Heading ,Image,Card, Description, Details, DisplayFlex} from "./style-events.js"
+//import {Event, Title ,Img, Cardrev, Heading ,Image,Card, Description, Details, DisplayFlex} from "./style-events.js"
+import styled from 'styled-components'
 import Footer from '../../components/Footer/Footer';
 
 function Events(){
@@ -50,4 +51,155 @@ function Events(){
     );
 }
 
-export default Events
+export default Events;
+
+
+const Container = styled.div`
+  display:block;
+  justify-content:center;
+  align-items:center;
+`;
+
+const DisplayFlex = styled.div`
+    padding:.2rem;
+    display: flex;
+    margin:2rem;
+    width:-webkit-fill-available;
+    align-items:center;
+    justify-content:center;
+    align-self:center;
+    overflow-x: hidden;
+    flex-direction:column;
+    @media (max-width: 1024px){
+        
+    }
+    @media (max-width: 768px) {
+        
+    }
+    @media (max-width: 576px) {
+      margin:.5rem;
+    }
+`;
+
+  const Card=styled.span`
+    display:flex;
+    width: 100%;
+    justify-content:center;
+    height: fit-content;
+    //background:#fff000;
+    border-radius:1rem;
+    align-items:center;
+    overflow:hidden;
+    margin:1rem;
+    @media screen and (max-width:900px)
+    {
+      flex-direction: column;
+      margin:.5rem;
+      margin-bottom:.7rem;
+    }
+    @media screen and (max-width:767px)
+    {
+      flex-direction:column;
+      margin:0rem;
+      margin-bottom:.7rem;
+    }
+`;
+
+  const Cardrev=styled.span`
+    display:flex;
+    width: 100%;
+    justify-content:center;
+    height: fit-content;
+    //background:#fff000;
+    align-items:center;
+    border-radius:1rem;
+    overflow:hidden;
+    margin:1rem;
+    
+    @media screen and (max-width:900px)
+    {
+      flex-direction: column-reverse;
+      margin:0rem;
+      margin-bottom:.7rem;
+    }
+
+    @media screen and (max-width:767px)
+    {
+      flex-direction: column-reverse;
+      margin:0rem;
+      margin-bottom:.7rem;
+    }
+`;
+
+  const Heading=styled.div`
+    font-size:3rem; 
+    color:#102E46;
+    margin-top: 4%;
+    text-align:center;
+    font-weight:bold;
+    font-family:san-serif;
+`;
+
+  const Details=styled.div`
+    color:grey;
+    font-size:1.1rem;
+    display:flex;
+    flex-wrap:wrap;
+    margin:0.5rem 1rem;
+    font-family:roboto;
+    @media (max-width: 576px) {
+      width:97%; 
+      margin:0.5rem 0.3rem;
+      font-size:1rem;
+    }
+`;
+
+  const Img=styled.div` 
+  margin:1rem;
+`;
+  const Image=styled.img` 
+  border-radius:15px;
+`;
+
+
+  const Title=styled.div`
+    font-family:"Quicksand",sans-serif;
+    flex-wrap:wrap;
+    font-size:1.5rem;
+    font-weight:bold;
+    margin: 1rem;
+    @media (max-width: 767px) {
+      width:97%; 
+      margin:0.5rem 0.3rem;
+      font-size:1.3rem;
+    }
+`;
+
+  const Description=styled.div`
+    display:flex;
+    justify-self:center;
+    flex-direction:column;
+    flex-wrap:wrap;
+    margin:0rem 1.5rem;
+    margin-bottom:.7rem;
+    width:60%;
+    border-radius: 10px;
+    background: #FFFFFF;
+    box-shadow:  15px 15px 30px #bababa,
+             -15px -15px 30px #ffffff;
+
+    @media (max-width: 767px) {
+        width:95%;
+    }         
+`;
+
+  const Event=styled.div`
+    display:"flex";
+    flex-direction:column;
+    align-items:center;
+    justify-content:center;
+    width:80%;
+    @media (max-width: 767px) {
+      width:100%; 
+  }   
+`;
--- pages/Team/index.jsx
+++ pages/Team/index.jsx
@@ -4,7 +4,8 @@ import React, { useState } from 'react'
 import {RiTwitterLine} from "react-icons/ri";
 import {FaGithubAlt} from "react-icons/fa"
 import {AiOutlineLinkedin} from "react-icons/ai"
-import {Container, DisplayFlex ,Name,Img,LineB,Job, Heading ,Card, Description, DescriptionContent, Profile, Social, Details, DisplayGrid} from "./team-styles.js"
+//import {Container, DisplayFlex ,Name,Img,LineB,Job, Heading ,Card, Description, DescriptionContent, Profile, Social, Details, DisplayGrid} from "./team-styles.js"
+import styled from 'styled-components'
 import teamdata from "../../data/team.json"
 import '../../styles/Home.module.css'
 import Footer from '../../components/Footer/Footer';
@@ -54,4 +55,169 @@ export default function Team(){
       </> 
       )
 };
-// export default Team;
+
+
+
+  const DisplayFlex = styled.div`
+    padding:.2rem;
+    display: flex;
+    margin: 2rem 10rem;
+    width:-webkit-fill-available;
+    align-items:center;
+    justify-content:center;
+    justify-self:center;
+    align-self:center;
+    overflow-x: hidden;
+    flex-direction:column;
+    
+    @media (max-width: 1200px){
+        margin: 2rem;
+    }
+    @media (max-width: 1024px){
+        margin: 2rem;
+    }
+    @media (max-width: 768px) {
+        margin: 2rem;
+    }
+    @media (max-width: 576px) {
+      margin:.5rem;
+    }
+`;
+
+
+  const DisplayGrid = styled.div`
+    padding:1rem;
+    display: grid;
+    overflow-x: hidden;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-column-gap: 2rem;
+    grid-row-gap:2rem;
+    @media (max-width: 1024px){
+        grid-template-columns: 1fr 1fr;
+        grid-column-gap: 1rem;
+    }
+    @media (max-width: 768px) {
+        grid-template-columns: 1fr;
+        padding:.8rem;
+    }
+    @media (max-width: 576px) {
+        grid-template-columns: 1fr;
+        padding: .5rem;
+    }
+`;
+
+  const Profile=styled.div`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`;
+  const Card=styled.span`
+    display:flex;
+    width: fit-content;
+    height: fit-content;
+    max-width:315px;
+    //border: 3px solid #102E46;
+`;
+  const Social=styled.span`
+   display:flex;
+   margin-left:.6rem;
+   flex-direction:column;
+   align-self:flex-end;
+   font-size:1.4rem;
+   color:dark-blue;
+`;
+  const Heading=styled.div`
+    font-size:3rem; 
+    color:#102E46;
+    margin-top:1rem;
+    padding-bottom:2rem;
+    text-align:center;
+    font-weight:bold;
+    font-family:san-serif;
+`;
+  const Details=styled.div`
+    color:grey;
+    font-size:1.1rem;
+    display:flex;
+    flex-direction:column;
+    align-self:left;
+    width:100%;
+    margin-left:.2rem;
+    font-family:roboto;
+    @media (max-width: 576px) {
+      width:100%; 
+    }
+`;
+  const Name=styled.p`
+font-weight:bold;
+color: #102E46;
+margin-block-start: 1em;
+margin-block-end: 0em;
+font-size:20px;
+@media (max-width: 576px){
+font-size:17px;}
+`;
+  const Job=styled.p`
+margin-block-start: 0em;
+margin-block-end: 0em;
+color: #102E46;
+font-size:15px;
+@media (max-width: 576px){
+font-size:15px;}
+`;
+  const LineB=styled.div`
+    height:9.9rem;
+    background:"#0000CD";
+    width:"3px" ;
+    marginTop:".6rem";
+    marginLeft:".6rem"; 
+    @media (max-width: 576px) {
+        height:7rem;
+    }
+`;
+  const Img=styled.div`
+    -webkit-clip-path: polygon(0 0, 0 100%, 100% 100%, 100% 25%, 75% 0);
+    clip-path: polygon(0 0, 0 100%, 100% 100%, 100% 25%, 75% 0);
+    border: 3px solid #102E46;
+    position: relative;
+`;
+
+  const DescriptionContent=styled.p`
+    font-size:1.25rem;
+    margin-top:0.15rem;
+    display:flex;
+    justify-content:center;
+`;
+
+
+  const Description=styled.span`
+    position: absolute;
+    opacity:60%;
+    top:0;
+    left:0;
+    width:100%;
+    height:100%;
+    background: rgba(0,0,0,0.6);
+    font-family: "Quicksand" ,sans-serif;
+    color:#ffffff;
+    font-weight:bold;
+    display:flex;
+    flex-direction: column;
+    justify-content:center;
+    backdrop-filter:blur(3px);
+    opacity:0;
+    transition: opacity 0.75s;
+    
+    p${DescriptionContent}{
+        transform: translateY(30px);
+        transition: transform 0.75s;
+    }
+    
+    :hover{
+        opacity:1;
+
+        p${DescriptionContent}{
+            transform: translateY(0px);
+        }
+    }
+`;
